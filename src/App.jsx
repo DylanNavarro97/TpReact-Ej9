d484d5d9389c54d016671328bbcc52e072e20021
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-02-05 05:32:54 -0300

    borrar formulario al enviar

--- src/App.jsx
+++ src/App.jsx
@@ -24,23 +24,32 @@ function App() {
     e.preventDefault();
     if (verificarFormulario()) {
       if (verificarUnicoTurno()) {
-        const citasArray = [...citas, formulario]
+        const citasArray = [...citas, formulario];
         setCitas(citasArray);
-        localStorage.setItem("citas", JSON.stringify(citasArray))
-        alert("Cita creada con exito")
+        localStorage.setItem("citas", JSON.stringify(citasArray));
+        alert("Cita creada con exito");
+        setFormulario({
+          nombreMascota: "",
+          nombreDueno: "",
+          fecha: "",
+          hora: "",
+          sintomas: "",
+        });
       } else {
-        alert("Esta veterinaria solo permite un turno por dueño")
+        alert("Esta veterinaria solo permite un turno por dueño");
       }
     } else {
-      alert("Completa los datos")
+      alert("Completa los datos");
     }
   };
 
   const handleErase = (citaProp) => {
-    const citasFiltradas = citas.filter((cita) => cita.nombreDueno !== citaProp.nombreDueno)
-    setCitas(citasFiltradas)
-    localStorage.setItem("citas", JSON.stringify(citasFiltradas))
-  }
+    const citasFiltradas = citas.filter(
+      (cita) => cita.nombreDueno !== citaProp.nombreDueno
+    );
+    setCitas(citasFiltradas);
+    localStorage.setItem("citas", JSON.stringify(citasFiltradas));
+  };
 
   const verificarFormulario = () => {
     if (
@@ -76,11 +85,10 @@ function App() {
     }
   };
 
-
   useEffect(() => {
-    const citasLS = JSON.parse(localStorage.getItem("citas")) || []
-      setCitas(citasLS)
-  },[])
+    const citasLS = JSON.parse(localStorage.getItem("citas")) || [];
+    setCitas(citasLS);
+  }, []);
 
   return (
     <section className="container">
@@ -99,9 +107,9 @@ function App() {
         <h4>Administra las citas aqui</h4>
 
         <div className="d-flex flex-wrap gap-5 justify-content-center my-5">
-            {citas?.map((cita, i) => (
-              <Card cita={cita} key={i} handleErase={handleErase}/>
-            ))}
+          {citas?.map((cita, i) => (
+            <Card cita={cita} key={i} handleErase={handleErase} />
+          ))}
         </div>
       </div>
     </section>
